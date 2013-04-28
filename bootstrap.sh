8c4717e22e988324bdba85622b59def9ec77f722
ruanhao <ruanhao1116@gmail.com>
2013-04-28 23:58:57 +0800

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,20 @@ VIMRC="$HOME/.vimrc"
 
 for i in $VIMDIR $VIMRC; do
     orig="${i}.orig"
-    if [[ ( -e $orig ) || ( -h $orig ) ]]; then
+    if [[ ( -e "$orig" ) || ( -h "$orig" ) ]]; then
         echo "removing $orig"
         rm -rf $orig
     fi
-    if [[ ( -e $i ) || ( -h $i ) ]]; then
+    
+    if [[ -h "$i" ]]; then 
+        if [[ -e "$( readlink $i )" ]]; then 
+            echo "renaming $i ---> $orig"
+            mv $i $orig
+        else 
+            echo "removing $i"
+            rm -f $i
+        fi
+    else 
         echo "renaming $i ---> $orig"
         mv $i $orig
     fi
@@ -29,7 +38,7 @@ echo "updating VIM Scripts"
 mkdir -p $VIMDIR/bundle
 cd $VIMDIR/bundle
 while read repo; do
-    [[ ( "${repo:0:1}" != "#" ) && ( "${#repo}" != "0" ) ]] && git clone $repo
+    [[ ( ${repo:0:1} != "#" ) && ( ${#repo} -ne 0 ) ]] && git clone $repo
 done <$VIMDIR/repo.config
 
 echo "It rocks, pls enjoy :)"
