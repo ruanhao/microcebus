31a6d38606ce682865059d07b30f847d70df2cbc
ruanhao <ryan.ruan@ericsson.com>
2013-04-28 10:56:54 +0800

    update repo.config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,31 @@
 #! /bin/bash
 
-tmpDir="$HOME/Microcebus"
-cloneCmd="git clone git@github.com:ruanhao/Microcebus.git $tmpDir"
-[[ -d "$tmpDir"  ]] && echo "rm -rf ${tmpDir} ..."; \
-                       rm -rf $tmpDir; ( exec $cloneCmd ) || ( exec $cloneCmd ) 
-cd $tmpDir
-echo $( pwd )
+origVimDir="$HOME/.vim.orig"
+vimDir="$HOME/.vim"
+[[ -d "$origVimDir" ]] && echo "removing $origVimDir"; \
+                          rm -rf $origVimDir
+
+if [[ -d "$vimDir" ]]; then
+    echo "renaming $vimDir ---> $origVimDir"
+    mv $vimDir $origVimDir
+    git clone  git@github.com:ruanhao/Microcebus.git $vimDir
+else
+    echo "creating $vimDir"
+    mkdir -p $vimDir
+    git clone  git@github.com:ruanhao/Microcebus.git $vimDir
+fi
+
+echo "updating pathogen file" 
+tmpDir=$( mktemp -d )
+git clone git@github.com:tpope/vim-pathogen.git $tmpDir > /dev/null
+cp -r $tmpDir/autoload $vimDir
+
+while read repo; do
+    echo $repo
+done <$vimDir/repo.config
+
+
+
+
+    
+
