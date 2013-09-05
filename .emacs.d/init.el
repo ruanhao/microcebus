d53b1e14b4ab7e29d358f457acb1506005cf5812
ruanhao <ryan.ruan@ericsson.com>
2013-09-05 17:54:47 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; set default mode
+;; Set Default mode
 (setq default-major-mode 'text-mode)
 
 ;; can't live without C-h
@@ -19,7 +19,13 @@
 ;; Add line number
 (require 'linum)
 (global-linum-mode t)
-(setq-default linum-format "%2d ")
+(setq-default linum-format
+	      (concat "%" 
+		      (number-to-string 
+		       (length (number-to-string 
+				(count-lines 
+				 (point-min) (point-max)))))
+		      "d "))
 
 ;; yes or no
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -68,3 +74,107 @@
 ;; use C-x w h to highlight and 
 ;; C-x w r to unhighlight
 (global-hi-lock-mode 1)
+
+
+;; just for fun
+(defun show-system-type ()
+  "find out what OS Emacs is currently running on"
+  (interactive)
+  (message "%s" system-type))
+
+(defun show-emacs-major-version ()
+  "find out Emacs major version"
+  (interactive)
+  (message "%s" emacs-major-version))
+
+(defun show-features-loaded ()
+  "find out all features loaded"
+  (interactive)
+  (message "%S" features))
+
+(defun highlight-current-word ()
+  "highlight the word under cursor"
+  (interactive)
+  (let (head-point tail-point word)
+    (skip-chars-forward "-_A-Za-z0-9")
+    (setq tail-point (point))
+    (skip-chars-backward "-_A-Za-z0-9")
+    (setq head-point (point))
+    (setq word (buffer-substring-no-properties head-point tail-point))
+    (highlight-regexp word 'hi-yellow)))
+
+
+(defun unhighlight-current-word ()
+  "unhighlight the word under cursor"
+  (interactive)
+  (let (head-point tail-point word)
+  (skip-chars-forward "-_A-Za-z0-9")
+  (setq tail-point (point))
+  (skip-chars-backward "-_A-Za-z0-9")
+  (setq head-point (point))
+  (setq word (buffer-substring-no-properties head-point tail-point))
+  (unhighlight-regexp word)))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;; useful functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (point)
+;; (region-beginning)
+;; (region-end)
+;; (line-beginning-position)
+;; (line-end-position)
+;; (point-min)
+;; (point-max)
+;; 
+;; (goto-char 1)
+;; (forward-char 1)
+;; (backward-char 1)
+;; 
+;; ;; move cursor to the location of "string"
+;; ;; returns the new position
+;; (search-forward "string")		; end of "string"
+;; (search-backward "string")		; beginning of "string"
+;; (re-search-forward "string")
+;; (re-search-backward "string")
+;; ;; move cursor to the first char that's not "a to z"
+;; ;; returns the distance traveled
+;; (skip-chars-forward "a-z")
+;; (skip-chars-backward "a-z")
+;; 
+;; (delete-char 1)
+;; (delete-region start-pos end-pos)
+;; (insert "string")
+;; ;; get string from current buffer
+;; (buffer-substring-no-properties start-pos end-pos)
+;; (capitalize-region start-pos end-pos)
+;; 
+;; (length "string")
+;; (substring "string" start-index end-index)
+;; (replace-regexp-in-string regex replacement str)
+;; 
+;; (buffer-name)
+;; (buffer-file-name)
+;; ;; switch to other buffer
+;; ;; this function does not display the buffer in any window, 
+;; ;; so the user cannot necessarily see the buffer. 
+;; ;; but Lisp programs will now operate on it. 
+;; (set-buffer buffer-name)
+;; (save-buffer)
+;; (kill-buffer buffer-name)
+;; (kill-this-buffer)
+;; ;; temporarily sets a buffer as current to work with
+;; (with-current-buffer buffer-name
+;;   ;; do something here)
+;; 
+;; ;; insert file into current position
+;; (insert-file-contents file-path)
+;; ;; append a text block to file
+;; (append-to-file start-pos end-pos file-path)
+;; ;; get dir path
+;; (file-name-directory full-file-path)
+;; ;; get filename part
+;; (file-name-nondirectory full-file-path)
+;; ;; get filename's suffix
+;; (file-name-extension file-name)
+;; (file-name-sans-extension file-name)
